2ec063cc6056a69982cc059d86b5f0c0dd06e57e
Vladimir Sitchikhin <volodya95rus@icloud.com>
2022-02-21 20:45:17 +0300

    v 1.0
    modell
    #message - закончил проект, но возможно придется допиливать выбор варианта (из методички) на этот случай есть закомментированный код))

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sympy
+import numpy as np
 from sympy import *
 import re
 
@@ -15,11 +16,25 @@ testsNumber = input()
 testsNumber = int(testsNumber)
 
 
+
+# print('Чему равно x*?')
+# xConverseMethod = input()
+# print('Чему равно y*?')
+# yConverseMethod = input()
+
+
+
+
 inputData = []
 diffNumbers = []
 diffs = []
-matrix = []
+matrixA = []
+matrixB = []
+matrixC = []
+detriminants = []
+multipliers = []
 I = ''
+finalFunction = ''
 
 
 for i in range(termsNumber):
@@ -36,14 +51,49 @@ for i in range(termsNumber):
 for i in range(termsNumber):
     inputData[i].pop(0)
 
+
 # ToDo дать пользователю выбор приведения выражения к линейному уравнению
+# match xConverseMethod:
+#     case 'x^j':
+#         for i in range(termsNumber - 1):
+#             for j in range(testsNumber):
+#                 inputData[i][j] = inputData[i][j] ** (i + 1)
+#     case 'sqrt(x)':
+#         for i in range(testsNumber):
+#             inputData[1][j] = math.sqrt(inputData[1][j])
+#     case '1/xj':
+#         for i in range(termsNumber - 1):
+#             for j in range(testsNumber):
+#                 inputData[i][j] = 1 / inputData[i][j]
+#     case 'xj':
+#         for i in range(termsNumber - 1):
+#             for j in range(testsNumber):
+#                 inputData[i][j] = inputData[i][j]
+#
+# match yConverseMethod:
+#     case 'y':
+#         for i in range(testsNumber):
+#             inputData[termsNumber - 1][i] = inputData[termsNumber - 1][i]
+#     case 'ln(y)':
+#         for i in range(testsNumber):
+#             inputData[termsNumber - 1][i] = math.log(inputData[termsNumber - 1][i])
+#     case '1/y':
+#         for i in range(testsNumber):
+#             inputData[termsNumber - 1][i] = 1 / inputData[termsNumber - 1][i]
+#     case 'y^2':
+#         for i in range(testsNumber):
+#             inputData[termsNumber - 1][i] = inputData[termsNumber - 1][i] ** 2
+#     case 'x/y':
+#         for i in range(testsNumber):
+#             inputData[termsNumber - 1][i] = inputData[0][i]
+
+
 for i in range(termsNumber):
     for j in range(testsNumber):
-        if (i + 1 == termsNumber):
-            inputData[i][j] = inputData[i][j] ** 2
-        else:
+        if i < termsNumber - 1:
             inputData[i][j] = inputData[i][j] ** (i + 1)
-
+        else:
+            inputData[i][j] = inputData[i][j] ** 2
 
 for i in range(termsNumber):
     diffNumbers.append('a' + str(i))
@@ -69,5 +119,65 @@ for i in range(termsNumber):
     a = 'a%s' % i
     diffs.append(diff(I[:], a))
     diffs[i] = str(diffs[i])
-print(diffs)
 
+
+def changeMatrix(string, termsNumber):
+    A = []
+    string = string.replace(' ', '')
+    string = string.split('*', termsNumber)
+
+    A.append(int(string[0]))
+
+    for i in range(termsNumber):
+       if i > 0:
+           A.append(int(string[i][2:]))
+    return A
+
+
+for i in range(termsNumber):
+    matrixA.append(changeMatrix(diffs[i], termsNumber + 1))
+    matrixB.append(matrixA[i][termsNumber])
+    del matrixA[i][termsNumber]
+
+
+matrixA = np.array(matrixA)
+
+
+for i in range(termsNumber):
+    matrixB[i] = -matrixB[i]
+
+
+detriminants.append(int(np.linalg.det(matrixA)))
+for i in range(termsNumber):
+    matrixC = matrixA
+    matrixC = np.array(matrixC)
+    for j in range(termsNumber):
+        matrixC[i][j] = matrixB[j]
+
+    detriminants.append(int(np.linalg.det(matrixC)))
+
+for i in range(termsNumber + 1):
+    if i > 0:
+        multipliers.append(detriminants[i]/detriminants[0])
+
+for i in range(termsNumber):
+    multipliers[i] = round(multipliers[i], 2)
+
+
+# if yConverseMethod == 'ln(y)':
+#     multipliers[0] = math.exp(multipliers[0])
+
+
+finalFunction = finalFunction + 'y = √('
+for i in range(termsNumber):
+    if i == 0:
+        finalFunction = finalFunction + str(multipliers[i])
+    else:
+        if multipliers[i] > 0:
+            finalFunction = finalFunction + ' + ' + str(multipliers[i]) + '*x^%s' %i
+        else:
+            finalFunction = finalFunction + ' - ' + str(multipliers[i]) + '*x^%s' %i
+finalFunction = finalFunction + ')'
+
+print('Математическая модель заданного выражения:')
+print(finalFunction)
