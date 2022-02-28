eedad69ae88dccdc1a5e6d9011cb70e6ec1607ca
Vladimir Sitchikhin <volodya95rus@icloud.com>
2022-02-28 20:49:38 +0300

    v 1.1
    #message - пофиксил баг с потерей нулевых значений параметров в частных производных

--- main.py
+++ main.py
@@ -2,27 +2,28 @@ import sympy
 import numpy as np
 from sympy import *
 import re
-
+# ToDo Неправильная обработка производных при нулевых значениях.
+# Если в частной производной отсутствует какой-либо параметр,
+# его значение не должно быть утеряно, а должно равняться 0.
 
 
 print('Нахождение математической модели функции вида: y = √(a0 + a1x + a2x^2 + ... + anx^n)')
 print('Введите количество слагаемых: ')
 termsNumber = input()
 termsNumber = int(termsNumber)
+# termsNumber = 3
 
 
 print('Введите количество тестовых данных: ')
 testsNumber = input()
 testsNumber = int(testsNumber)
-
+# testsNumber = 3
 
 
 # print('Чему равно x*?')
 # xConverseMethod = input()
 # print('Чему равно y*?')
 # yConverseMethod = input()
-
-
 
 
 inputData = []
@@ -37,6 +38,7 @@ I = ''
 finalFunction = ''
 
 
+# Заполнение таблицы входных данных
 for i in range(termsNumber):
     if i + 1 == termsNumber:
         inputData.append(['y'])
@@ -46,13 +48,13 @@ for i in range(termsNumber):
         if j == 0:
             print('Введите слагаемые: ', inputData[i][0])
         inputData[i].append(int(input()))
-
+# inputData = [[1, 0, 0], [0, 1, 0], [1, 1, 1]]
 
 for i in range(termsNumber):
     inputData[i].pop(0)
 
 
-# ToDo дать пользователю выбор приведения выражения к линейному уравнению
+# Возможность дать пользователю выбор приведения выражения к линейному уравнению
 # match xConverseMethod:
 #     case 'x^j':
 #         for i in range(termsNumber - 1):
@@ -88,6 +90,7 @@ for i in range(termsNumber):
 #             inputData[termsNumber - 1][i] = inputData[0][i]
 
 
+#  Преобразование значений
 for i in range(termsNumber):
     for j in range(testsNumber):
         if i < termsNumber - 1:
@@ -95,10 +98,13 @@ for i in range(termsNumber):
         else:
             inputData[i][j] = inputData[i][j] ** 2
 
+
+# Заполнение массива параметров
 for i in range(termsNumber):
     diffNumbers.append('a' + str(i))
 
 
+# Приведение входных данных к функции ошибки (I)
 for k in range(testsNumber):
     I = I + '('
     for i in range(termsNumber + 1):
@@ -115,26 +121,53 @@ for k in range(testsNumber):
         I = I + ')**2 + '
 
 
+# Заполнение массива частных производных по каждому параметру
 for i in range(termsNumber):
     a = 'a%s' % i
     diffs.append(diff(I[:], a))
     diffs[i] = str(diffs[i])
 
 
-def changeMatrix(string, termsNumber):
-    A = []
+# Приведение полученных частных производных к необходимому виду для "вытаскивания" из них нужных значений
+def changeDiffs(string):
     string = string.replace(' ', '')
     string = string.split('*', termsNumber)
+    length = len(string)
+    return string, length
 
+
+# Поиск позиций "потерявшихся" параметров
+def searchLostArgs(string, termsNumber):
+    lostCounter = 0
+    if len(string) < termsNumber + 1:
+        for i in range(len(diffNumbers)):
+            for j in range(len(string)):
+                if not diffNumbers[i] in string[j]:
+                    lostCounter += 1
+            if lostCounter >= len(string):
+                string.insert(i, '0')
+            lostCounter = 0
+    return string
+
+# Приведение массива частных производных к матрице
+def changeMatrix(string, termsNumber):
+    A = []
     A.append(int(string[0]))
 
     for i in range(termsNumber):
        if i > 0:
-           A.append(int(string[i][2:]))
+           if 'a' in string[i]:
+            A.append(int(string[i][2:]))
+           else:
+            A.append(int(string[i]))
+
     return A
 
 
+#  Заполнение матриц А и В
 for i in range(termsNumber):
+    diffs[i], length = changeDiffs(diffs[i])
+    diffs[i] = searchLostArgs(diffs[i], termsNumber)
     matrixA.append(changeMatrix(diffs[i], termsNumber + 1))
     matrixB.append(matrixA[i][termsNumber])
     del matrixA[i][termsNumber]
@@ -147,19 +180,23 @@ for i in range(termsNumber):
     matrixB[i] = -matrixB[i]
 
 
+# Заполнение массива определителей
 detriminants.append(int(np.linalg.det(matrixA)))
 for i in range(termsNumber):
     matrixC = matrixA
     matrixC = np.array(matrixC)
     for j in range(termsNumber):
-        matrixC[i][j] = matrixB[j]
-
+        matrixC[j][i] = matrixB[j]
     detriminants.append(int(np.linalg.det(matrixC)))
 
+
+# Заполнение массива итоговых значений параметров
 for i in range(termsNumber + 1):
     if i > 0:
         multipliers.append(detriminants[i]/detriminants[0])
 
+
+# Округление итоговых значений параметров
 for i in range(termsNumber):
     multipliers[i] = round(multipliers[i], 2)
 
@@ -168,6 +205,7 @@ for i in range(termsNumber):
 #     multipliers[0] = math.exp(multipliers[0])
 
 
+# Составление и вывод итоговой функции (математической модели)
 finalFunction = finalFunction + 'y = √('
 for i in range(termsNumber):
     if i == 0:
@@ -178,6 +216,7 @@ for i in range(termsNumber):
         else:
             finalFunction = finalFunction + ' - ' + str(multipliers[i]) + '*x^%s' %i
 finalFunction = finalFunction + ')'
+
 
 print('Математическая модель заданного выражения:')
 print(finalFunction)
